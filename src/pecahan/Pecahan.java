58a015e1b46751a2c0674ffe162fb26eb7eaf439
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 20:59:32 +0700

    Add files via upload

---
+++ src/pecahan/Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pecahan;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1 
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis"
+ * tipe data bilangan pecahan
+ * 
+ */
+
+public class Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 123456789.0123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("FLoat : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
